70e94f3fb9153dd808d9e453260486825d43b9f3
vishalmishra4u <mishravishal0912@gmail.com>
2019-02-14 23:47:25 +0530

    MaxChars solution added

--- MaxChars/maxChars.js
+++ MaxChars/maxChars.js
@@ -1,5 +1,7 @@
 function maxChars(str){
   const chars = {};
+  let max = 0;
+  let maxChar = '';
 
   for(let char of str){
     if(!chars[char]){
@@ -8,6 +10,15 @@ function maxChars(str){
       chars[char]++;
     }
   }
+
+  for(let char in chars){
+    if(chars[char] > max){
+      max = chars[char];
+      maxChar = char;
+    }
+  }
+
+  return maxChar;
 }
 
 module.exports = maxChars;
--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 var assert = require('assert'),
   reverse = require('./../StringReversal/stringReversal2'),
   palindrome = require('./../Palindrome/palindrome2');
+  maxChars = require('./../MaxChars/maxChars');
 
 describe('reverse()', function() {
   it('should return the reverse of the passed string', function() {
@@ -17,5 +18,14 @@ describe('palindrome()', function() {
   });
   it('should return true if the passed string is a palindrome, else false', function() {
     assert.equal(palindrome('abba'), true);
+  });
+});
+
+describe('palindrome()', function() {
+  it('should return max number of chars used in the string', function() {
+    assert.equal(maxChars('apple'), "p");
+  });
+  it('should return max number of chars used in the string', function() {
+    assert.equal(maxChars('abbcc 1122233333'), "3");
   });
 });

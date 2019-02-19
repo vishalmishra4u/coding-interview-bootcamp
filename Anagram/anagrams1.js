d8ddc670015fa99a878575629d993031c6d3dfa8
vishalmishra4u <mishravishal0912@gmail.com>
2019-02-19 20:42:42 +0530

    ANagram problem and solution added

---
+++ Anagram/anagrams1.js
@@ -0,0 +1,29 @@
+function anagrams(stringA, stringB){
+  const aCharMap = buildCharMap(stringA);
+  const bCharMap = buildCharMap(stringB);
+
+  if(Object.keys(aCharMap).length !== Object.keys(bCharMap).length){
+    return false;
+  }
+
+  for(let char in aCharMap){
+    if(aCharMap[char] !== bCharMap[char]){
+      return true;
+    }
+  }
+
+  return true;
+}
+
+//Builds and return object map of a string
+function buildCharMap(str){
+  const charMap = {};
+
+  for(let char of str.replace(/[^\w]/g,[]).toLowerCase()){
+    charMap[char] = charMap[char] + 1 || 1;
+  }
+
+  return charMap;
+}
+
+module.exports = anagrams;
